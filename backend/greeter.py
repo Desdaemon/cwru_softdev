60a3adf7b45b56af53d51a5d3e0557244ebee3e9
Viet Dinh <54ckb0y789@gmail.com>
2022-02-07 11:01:39 -0500

    mypy types, refactoring

--- backend/greeter.py
+++ backend/greeter.py
@@ -1,26 +1,25 @@
 import grpc
 import sys
 from concurrent import futures
-import greeter_pb2 as pb
-import greeter_pb2_grpc as rpc
+from generated import greeter_pb2, greeter_pb2_grpc
 import logging
 
 PORT = 50051
 
-class GreeterServicer(rpc.GreeterServicer):
-    def sayHello(self, request, context):
+class GreeterServicer(greeter_pb2_grpc.GreeterServicer):
+    def sayHello(self, request: greeter_pb2.HelloRequest, _: grpc.ServicerContext) -> greeter_pb2.HelloReply:
         logging.info(request)
         name = request.name or "stranger"
-        return pb.HelloReply(message=f"Hello there, {name}.",)
+        return greeter_pb2.HelloReply(message=f"Hello there, {name}.")
 
-    def sayHelloAgain(self, request, context):
+    def sayHelloAgain(self, request: greeter_pb2.HelloRequest, _: grpc.ServicerContext) -> greeter_pb2.HelloReply:
         logging.info(request)
         name = request.name or "stranger"
-        return pb.HelloReply(message=f"Hello again, {name}. How you doin'?")
+        return greeter_pb2.HelloReply(message=f"Hello again, {name}. How you doin'?")
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    rpc.add_GreeterServicer_to_server(GreeterServicer(), server)
+    greeter_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
     server.add_insecure_port(f'[::]:{PORT}')
     server.start()
     logging.info(f'Server starting on port {PORT}')
--- justfile
+++ justfile
@@ -1,11 +1,27 @@
 # To use this file, install Just: https://github.com/casey/just
 
+gen: gen-dart gen-py
+
 gen-dart:
     protoc -Iprotos protos/*.proto --dart_out=grpc:lib/generated
-gen-py:
-    python3 -m grpc_tools.protoc -Iprotos protos/*.proto \
-            --python_out=backend \
-            --grpc_python_out=backend
+
+setup-py pybin="python3":
+    {{pybin}} -m pip install mypy-protobuf grpcio-tools
+
+gen-py pybin="python3":
+    {{pybin}} -m grpc_tools.protoc -Iprotos protos/*.proto \
+            --python_out=backend/generated \
+            --grpc_python_out=backend/generated \
+            --mypy_out=backend/generated \
+            --mypy_grpc_out=backend/generated
+
+clean:
+    flutter clean
+    rm -r backend/generated
+    mkdir -p backend/generated
+    rm -r lib/generated
+    mkdir -p lib/generated
+
 lint:
     # exclude protobuf files from formatting
     dart format -l 120 $(find . -name "*.dart" -not -name "*.pb*.dart")
