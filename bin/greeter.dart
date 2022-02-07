4b1517969e00b1acf7479e16cc7137d323289e87
Viet Dinh <54ckb0y789@gmail.com>
2022-02-07 00:19:48 -0500

    Finish gRPC demo

---
+++ bin/greeter.dart
@@ -0,0 +1,16 @@
+import 'package:cwru_softdev/generated/greeter.pbgrpc.dart';
+import 'package:grpc/grpc.dart';
+
+Future<void> main() async {
+  final channel = ClientChannel(
+    '127.0.0.1',
+    port: 50051,
+    options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
+  );
+  final api = GreeterClient(channel);
+  await api.sayHello(HelloRequest()).then(print);
+  await api.sayHello(HelloRequest(name: 'John')).then(print);
+  await api.sayHelloAgain(HelloRequest()).then(print);
+  await api.sayHelloAgain(HelloRequest(name: 'John')).then(print);
+  await channel.shutdown();
+}
