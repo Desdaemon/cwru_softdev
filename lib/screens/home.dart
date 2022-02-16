c98856b39b134427395829cd103ef0242e5fc871
Viet Dinh <54ckb0y789@gmail.com>
2022-02-15 23:13:02 -0500

    Use MapBox

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,15 +2,61 @@ import 'package:cwru_softdev/widgets/cached_tile_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:blur/blur.dart';
+
+const _accessTokenEnv = 'mapbox_token';
+const _accessToken = String.fromEnvironment(_accessTokenEnv);
+
+enum Layers { dark, light }
+
+extension LayerExt on Layers {
+  String get url => const {
+        Layers.dark: 'https://api.mapbox.com/styles/v1/mapbox/navigation-night-v1/tiles/{z}/{x}/{y}{r}'
+            '?access_token=$_accessToken',
+        Layers.light: 'https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/tiles/{z}/{x}/{y}{r}'
+            '?access_token=$_accessToken'
+      }[this]!;
+}
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key})
+      : assert(const bool.hasEnvironment(_accessTokenEnv),
+            'MapBox token not provided' "Define one by using '--dart-define=$_accessTokenEnv=..'"),
+        super(key: key);
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  static const _maxZoom = 18.0;
+  static const _minZoom = 1.0;
+
   int _counter = 0;
+  bool _controllerReady = false;
+
+  bool get _isDark => MediaQuery.of(context).platformBrightness == Brightness.dark;
+  String get _url => _isDark ? Layers.dark.url : Layers.light.url;
+
+  late MapController _controller;
+
+  double get _zoom => _controllerReady ? _controller.zoom : 16;
+
+  void _zoomTo(double zoom) {
+    setState(() {
+      _controller.move(_controller.center, zoom.clamp(_minZoom, _maxZoom));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = MapController()
+      ..onReady.then((_) {
+        setState(() {
+          _controllerReady = true;
+        });
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,21 +64,68 @@ class _HomePageState extends State<HomePage> {
       body: FlutterMap(
         // CWRU's position
         options: MapOptions(
-          center: LatLng(41.5035702, -81.6124676),
-          zoom: 15,
-          maxZoom: 17,
+          center: LatLng(41.5043453, -81.6105725),
+          zoom: _zoom,
+          maxZoom: _maxZoom,
+          minZoom: _minZoom,
+          enableScrollWheel: false,
         ),
+        mapController: _controller,
         layers: [
           TileLayerOptions(
+            tileSize: 512,
+            zoomOffset: -1,
             tileProvider: const CachedTileProvider(),
-            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            subdomains: const ['a', 'b', 'c'],
-            attributionBuilder: (_) {
-              return const Text("© OpenStreetMap contributors", style: TextStyle(color: Colors.black));
-            },
+            urlTemplate: _url,
+            backgroundColor: Theme.of(context).backgroundColor,
           ),
         ],
+        nonRotatedChildren: [
+          Stack(
+            children: [
+              Positioned(
+                right: 12,
+                bottom: 12,
+                child: Column(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        _zoomTo(_zoom + 0.25);
+                      },
+                    ),
+                    StreamBuilder(
+                        // listen to events emitted by the controller as well
+                        stream: _controller.mapEventStream,
+                        builder: (context, _) {
+                          return RotatedBox(
+                            quarterTurns: 3,
+                            child: Slider(
+                              max: _maxZoom,
+                              min: _minZoom,
+                              value: _zoom,
+                              onChanged: _zoomTo,
+                            ),
+                          );
+                        }),
+                    IconButton(
+                      icon: const Icon(Icons.remove),
+                      onPressed: () {
+                        _zoomTo(_zoom - 0.25);
+                      },
+                    ),
+                  ],
+                ).frosted(
+                  frostColor: Theme.of(context).backgroundColor,
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniCenterFloat,
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
