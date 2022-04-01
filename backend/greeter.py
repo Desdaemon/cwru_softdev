920229acfa657b6a4bbbc7401ed7219b6cfadd74
Viet Dinh <54ckb0y789@gmail.com>
2022-04-01 13:47:36 -0400

    implement: register login tripsOf

--- backend/greeter.py
+++ backend/greeter.py
@@ -8,21 +8,69 @@ from generated import greeter_pb2_grpc
 from generated.greeter_pb2 import *
 
 PORT = 50051
-CONN = None
+DBPATH = 'test.db'
+
+def prepareDatabase(dbpath: str):
+    with open(dbpath, 'w') as _:
+        pass # truncate and close immediately
+
+def sql(query: str, params = ()):
+    res = []
+    with sqlite3.connect(DBPATH) as con:
+        res = con.execute(query, params).fetchall()
+    return res
 
 class UsersServicer(greeter_pb2_grpc.UsersServicer):
     def register(self, request: RegisterRequest, context: grpc.ServicerContext) -> User:
-        logging.info(request);
-        return super().register(request, context)
+        username = request.username
+        email = request.email
+        password = request.password
+        logging.info((username, email))
+        sql('insert into Users(username, email, password) values(?, ?, ?)', (username, email, password))
+        new_user = sql(
+            """
+            select user_id from Users
+            where username = ?
+            and   email = ? 
+            """,
+            (username, email))
+        assert len(new_user) == 1, 'failed to find new user'
+        user_id, = new_user[0]
+        return User(user_id=int(user_id), username=username, email=email)
 
     def login(self, request: LoginRequest, context: grpc.ServicerContext) -> User:
-        logging.info(request);
-        return super().login(request, context)
+        identity = request.identity
+        password = request.password
+        logging.info(f'login request for {identity}')
+        login_user = sql(
+            """
+            select user_id, username, email from Users
+            where (username = ? or email = ?)
+            and   password = ?
+            """,
+            (identity, identity, password))
+        assert len(login_user) == 1, 'failed to login'
+        user_id, username, email = login_user[0]
+        return User(user_id=int(user_id), username=username, email=email)
 
 
 class TripsServicer(greeter_pb2_grpc.TripsServicer):
     def tripsOf(self, request: TripsOfRequest, context: grpc.ServicerContext) -> TripsOfResponse:
-        logging.info(request);
+        userid = request.user_id
+        logging.info(f'retrieve trips for {userid}')
+        stops = sql(
+            """
+            select T.trip_id, D.lat, D.lon
+            from Trips T
+            inner join TripDestinations TD on
+                T.user_id = ? and
+                T.trip_id = TD.trip_id
+            natural join Destinations D
+            group by T.trip_id
+            order by visit_date asc, visit_time asc
+            """,
+            (userid,))
+        logging.info(stops)
         return super().tripsOf(request, context)
 
     def photosOf(self, request: PhotosOfRequest, context: grpc.ServicerContext) -> Iterator[PhotosOfResponse]:
@@ -43,7 +91,11 @@ class TripsServicer(greeter_pb2_grpc.TripsServicer):
 
 
 def serve():
-    CONN = sqlite3.connect("data.db")
+    prepareDatabase(DBPATH)
+    with open('schema.sql', 'r') as f:
+        with sqlite3.connect(DBPATH) as con:
+            con.executescript(f.read())
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     greeter_pb2_grpc.add_TripsServicer_to_server(TripsServicer(), server)
     greeter_pb2_grpc.add_UsersServicer_to_server(UsersServicer(), server)
@@ -54,6 +106,7 @@ def serve():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format='[%(asctime)s %(levelname)s] (%(funcName)s) %(message)s')
     serve()
 
--- backend/schema.sql
+++ backend/schema.sql
@@ -1,40 +1,40 @@
-create table Users (
-  user_id int primary key not null,
-  username varchar,
+create table Users(
+  user_id integer not null unique primary key autoincrement,
+  username varchar unique,
   email varchar,
   password varchar
 );
 
-create table Trips (
-  trip_id int primary key not null,
+create table Trips(
+  trip_id integer not null unique primary key autoincrement,
   start_date date,
   end_date date,
-  user_id int,
-  foreign key(user_id) references Users(user_id)
+  user_id integer,
+  foreign key (user_id) references Users(user_id)
 );
 
 create table Photos(
-  photo_id int primary key not null,
+  photo_id integer not null unique primary key autoincrement,
   name varchar,
   url varchar,
-  date_taken date,
+  date_taken date
 );
 
 create table Destinations(
   lat double not null,
-  lon double not null,
+  lon decimal not null,
   visit_date date,
-  visit_time time,
-  description char,
+  visit_time datetime,
+  description text,
   primary key (lat, lon)
 );
 
 -- Relationships
 
 create table TripDestinations(
-  trip_id int not null,
-  lat double not null,
-  lon double not null,
+  trip_id integer not null,
+  lat decimal not null,
+  lon decimal not null,
   primary key (trip_id, lat, lon),
   foreign key (trip_id) references Trips(trip_id),
   foreign key (lat, lon) references Destinations(lat, lon)
@@ -42,20 +42,25 @@ create table TripDestinations(
 
 -- Mock data
 
-insert into Users values (1, "foo", "foo@foo.com", "hunter2");
-insert into Users values (2, "bar", "bar@foo.com", "hunter3");
+/*
+insert into Users values
+  (1, 'foo', 'foo@foo.com', 'hunter2'),
+  (2, 'bar', 'bar@foo.com', 'hunter3');
 
-insert into Trips values (1, "1998-02-04", "1998-03-05", 2);
-insert into Trips values (2, "2001-05-04", "2002-01-01", 1);
+insert into Trips values
+  (1, '1998-02-04', '1998-03-05', 2),
+  (2, '2001-05-04', '2002-01-01', 1);
 
-insert into Destinations values (42.42, -12.12, "1998-02-04", "23:59:59", null);
-insert into Destinations values (42.72, -12.13, "1998-02-06", "23:59:59", null);
-insert into Destinations values (42.92, -12.13, "1998-02-06", "23:59:59", null);
+insert into Destinations values
+  (42, -12.12, '1998-02-04', '23:59:59', null),
+  (42.72, -12.13, '1998-02-06', '23:59:59', null),
+  (42.92, -12.13, '1998-02-06', '23:59:59', null);
 
-insert into TripDestinations values (1, 42.42, -12.12);
-insert into TripDestinations values (1, 42.72, -12.12);
+insert into TripDestinations values
+  (1, 42.42, -12.12),
+  (1, 42.72, -12.12);
 
-insert into Photos values (1, "Clowns at the Circus", "https://donttrythisathome.clown/196b3/98/02/12","1998-02-12")
-insert into Photos values (4, "Capybaras are cute", "https://cpaps.deadext/4326b7dkk9/01/09/06","2001-09-06")
-
-
+insert into Photos values
+  (1, 'Clowns at the Circus', 'https://donttrythisathome.clown/196b3/98/02/12', '1998-02-12'),
+  (4, 'Capybaras are cute', 'https://cpaps.deadext/4326b7dkk9/01/09/06', '2001-09-06');
+*/
