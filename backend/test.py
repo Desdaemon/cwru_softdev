0773925342c594faa66539ec8d04cb7cc76d0b71
Viet Dinh <54ckb0y789@gmail.com>
2022-04-22 15:33:52 -0400

    Fill in all test cases for Users

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,12 @@
+from typing import Any
 import unittest
 from generated.greeter_pb2 import *
 
 from main import TripsServicer, UsersServicer, prepareDb
 
 TEST_DB = 'test.db'
+
+CTX: Any = None
 
 
 class UsersTests(unittest.TestCase):
@@ -17,29 +20,56 @@ class UsersTests(unittest.TestCase):
             username='foobar',
             email='food@food.com',
             password='hunter3'
-        ), None)
-        self.assertIsNotNone(user.user)
+        ), CTX)
         self.assertEqual(user.user.username, 'foobar')
         self.assertEqual(user.user.email, 'food@food.com')
 
-    def test_register_null_value(self):
-        pass
-
-    def test_register_invalid_value(self):
-        pass
+    def test_register_existing_username(self):
+        try:
+            self.users.Register(RegisterRequest(
+                username='foo',
+                email='foo@foo.com',
+                password='hunter2'
+            ), CTX)
+            self.fail('failed to catch registering against user foo')
+        except:
+            pass
 
     def test_login(self):
         user = self.users.Login(LoginRequest(
             identity='foo',
             password='hunter2'
-        ), None)
-        self.assertIsNotNone(user.user)
+        ), CTX)
+        self.assertEqual(user.user.username, 'foo')
+        user2 = self.users.Login(LoginRequest(
+            identity='foo@foo.com',
+            password='hunter2',
+        ), CTX)
+        self.assertEqual(user2.user.email, 'foo@foo.com')
+        # if fail, please change the database
+        self.assertEqual(user.user.user_id, user2.user.user_id)
 
     def test_login_null_value(self):
-        pass
+        user = self.users.Login(LoginRequest(), CTX)
+        # in gRPC, no attributes are ever null
+        # except when we pass null ourselves
+        # so just assert that we DID pass an error message
+        self.assertTrue(user.error)
+        user = self.users.Login(LoginRequest(
+            identity='foo'
+        ), CTX)
+        self.assertTrue(user.error)
+        user = self.users.Login(LoginRequest(
+            password='foo'
+        ), CTX)
+        self.assertTrue(user.error)
 
     def test_login_invalid_value(self):
-        pass
+        user = self.users.Login(LoginRequest(
+            identity='bogus',
+            password='bogus'
+        ), CTX)
+        self.assertTrue(user.error)
 
 
 class TripsTest(unittest.TestCase):
